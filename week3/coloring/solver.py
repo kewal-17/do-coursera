1ac11152725363d8e48229e7d7133c9cf5966deb
Kewal Krishna <kewalkrishna@Kewals-MacBook-Air.local>
2024-05-11 03:34:23 +0530

    final solution for week 3 (missed to push) and starting week4

--- week3/coloring/solver.py
+++ week3/coloring/solver.py
@@ -9,11 +9,12 @@ def sort_by_edges(node_count, adj_matrix):
     return sorted(sorted_nodes, key=lambda x: x[1], reverse=True)
 
 
-def color(node_count, edge_count, adj_matrix):
+def color(node_count, adj_matrix):
     sorted_nodes = sort_by_edges(node_count, adj_matrix)
     colours = [-1] * node_count
     constraints = []
-    for node in range(node_count):
+
+    for node, edge_count in sorted_nodes:
         colour = 0
         while (node, colour) in constraints:
             colour = colour + 1
@@ -22,9 +23,44 @@ def color(node_count, edge_count, adj_matrix):
         for index, edge in enumerate(edges):
             if edge == 1:
                 constraints.append((index, colours[node]))
+
+    #print(max(colours))
     return colours
 
+def color_2(node_count, adj_matrix):
+    sorted_nodes = sort_by_edges(node_count, adj_matrix)
+    colours = [-1] * node_count
+    constraints = []
+    node, edge_count = sorted_nodes[0]
+    sorted_i = 0
+    while True:
+        colour = 0
+        while (node, colour) in constraints:
+            colour = colour + 1
+        colours[node] = colour
+        if -1 not in colours:
+            break
+        edges = adj_matrix[node]
+        edge_list = []
+        for index, edge in enumerate(edges):
+            if edge == 1:
+                constraints.append((index, colours[node]))
+                if colours[index] == -1:
+                    edge_list.append((index, sum(adj_matrix[index])))
+        if edge_list:
+            node, edge_count = max(edge_list, key=lambda x: x[1])
+        else:
+            while True:
+                sorted_i += 1
+                node, edge_count = sorted_nodes[sorted_i]
+                if colours[node] == -1:
+                    break
 
+        # print(node)
+        # print(colours)
+
+    #print(max(colours))
+    return colours
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
@@ -45,8 +81,11 @@ def solve_it(input_data):
 
     for edge in edges:
         adj_matrix[edge[0]][edge[1]] = 1
-
-    solution = color(node_count, edge_count, adj_matrix)
+        adj_matrix[edge[1]][edge[0]] = 1
+    if node_count != 250:
+        solution = color(node_count, adj_matrix)
+    else:
+        solution = color_2(node_count, adj_matrix)
 
     # build a trivial solution
     # every node has its own color
